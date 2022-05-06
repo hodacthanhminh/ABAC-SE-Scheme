0d1bc402247d6a0fdac0408f9dc45743535637b8
thanhminh.hodac <thanhminh.hodac@m.citynow.vn>
2022-05-06 20:13:52 +0700

    update KNN functions

--- knn.py
+++ knn.py
@@ -1,35 +1,30 @@
+from random import random
 import numpy as np
+from utils import *
 
-def genInvertedMatrix(k):
-    while True:
-        Matrix = np.random.randint(1,4,[k,k])
-        if np.linalg.matrix_rank(Matrix) == k:
-             return Matrix
-
-def key(k): 
+def Key(k): 
     M1 = genInvertedMatrix(k)
     M2 = genInvertedMatrix(k)
     S = np.random.randint(2,size=k)
-
     return [M1,M2,S]
 
 def EncI(p,sk):
-    [M1,M2,S] = sk
+    (M1,M2,S) = sk
     p_a=[]
     p_b=[]
     for i,value in enumerate(S):
         val_a = val_b = 0
         if value == 0:
-            val_a = val_b = p[i]
+            val_a = p[i]
+            val_b = p[i]
         else:
-            r = np.random.rand()
+            r = np.random.rand()*10
             val_a = p[i]*0.5 + r 
             val_b = p[i]*0.5 - r 
         p_a.append(val_a)
         p_b.append(val_b)
-    p_encrypt_a = np.matmul(M1,p_a)
-    p_encrypt_b = np.matmul(M2,p_b)
-
+    p_encrypt_a = np.matmul(p_a,np.transpose(M1))
+    p_encrypt_b = np.matmul(p_b,np.transpose(M2))
     return [p_encrypt_a, p_encrypt_b]
 
 def EncQ(q,sk):
@@ -43,22 +38,17 @@ def EncQ(q,sk):
         if value == 1:
             val_a = val_b = q[i]
         else:
-            r = np.random.rand()
+            r = np.random.rand()*10
             val_a = q[i]*0.5 + r 
             val_b = q[i]*0.5 - r 
         q_a.append(val_a)
         q_b.append(val_b)
-    q_encrypt_a = np.matmul(np.transpose(invert_M1),q_a)
-    q_encrypt_b = np.matmul(np.transpose(invert_M2),q_b)
-
+    q_encrypt_a = np.matmul(q_a,invert_M1)
+    q_encrypt_b = np.matmul(q_b,invert_M2)
     return [q_encrypt_a, q_encrypt_b]
 
 def Search(I,Q): 
-    [p_ea, p_eb] = I
-    [q_ea, q_eb] = Q
-
-    return np.inner(q_ea,p_ea) + np.inner(q_eb,p_eb)
-
+    return np.inner(I[0],Q[0]) + np.inner(I[1],Q[1])
     
     
 
