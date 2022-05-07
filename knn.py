5c89e684f80645a56205f873ccb5feb4fd91fa68
thanhminh.hodac <thanhminh.hodac@m.citynow.vn>
2022-05-07 15:25:39 +0700

    using normal distribution N(3,4) for spliting random

--- knn.py
+++ knn.py
@@ -6,7 +6,7 @@ def Key(k):
     M1 = genInvertedMatrix(k)
     M2 = genInvertedMatrix(k)
     S = np.random.randint(2,size=k)
-    return [M1,M2,S]
+    return (M1,M2,S)
 
 def EncI(p,sk):
     (M1,M2,S) = sk
@@ -18,17 +18,17 @@ def EncI(p,sk):
             val_a = p[i]
             val_b = p[i]
         else:
-            r = np.random.rand()*10
+            r = 3 + np.random.rand()*4
             val_a = p[i]*0.5 + r 
             val_b = p[i]*0.5 - r 
         p_a.append(val_a)
         p_b.append(val_b)
     p_encrypt_a = np.matmul(p_a,np.transpose(M1))
     p_encrypt_b = np.matmul(p_b,np.transpose(M2))
-    return [p_encrypt_a, p_encrypt_b]
+    return (p_encrypt_a, p_encrypt_b)
 
 def EncQ(q,sk):
-    [M1,M2,S] = sk
+    (M1,M2,S) = sk
     q_a=[]
     q_b=[]
     invert_M1 = np.linalg.inv(M1)
@@ -38,14 +38,14 @@ def EncQ(q,sk):
         if value == 1:
             val_a = val_b = q[i]
         else:
-            r = np.random.rand()*10
+            r = 3 + np.random.rand()*4
             val_a = q[i]*0.5 + r 
             val_b = q[i]*0.5 - r 
         q_a.append(val_a)
         q_b.append(val_b)
     q_encrypt_a = np.matmul(q_a,invert_M1)
     q_encrypt_b = np.matmul(q_b,invert_M2)
-    return [q_encrypt_a, q_encrypt_b]
+    return (q_encrypt_a, q_encrypt_b)
 
 def Search(I,Q): 
     return np.inner(I[0],Q[0]) + np.inner(I[1],Q[1])
