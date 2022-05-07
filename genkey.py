b5309518742b74d190c2380a4159bf289749d5ad
thanhminh.hodac <thanhminh.hodac@m.citynow.vn>
2022-05-07 15:22:27 +0700

    using utils createHashKey

--- genkey.py
+++ genkey.py
@@ -5,7 +5,7 @@ from utils import *
 
 def genkey(k):
     sk = knn.Key(d)
-    KF = secrets.token_bytes(int(k/8))
-    Dummies = createDummys(L)
+    Dummies = createDummies(L)
     Primes = createPrimes(L)  
+    KF = createHashKey(Dummies)
     return (sk,KF,Dummies, Primes)
