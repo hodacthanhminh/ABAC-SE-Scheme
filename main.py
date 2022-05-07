1e8026a2c020be469ae07ddf4028167000286673
thanhminh.hodac <thanhminh.hodac@m.citynow.vn>
2022-05-07 16:35:17 +0700

    update constant and main

--- main.py
+++ main.py
@@ -1,93 +1,54 @@
-from operator import invert
-from genkey import *
-from hash import *
+# libs
 import numpy as np
+# constant
+from CONSTANT import *
+# function
+import knn
+from genkey import *
+from buildindex import *
+from trapdoor import *
 
-key_length = 5
-
-secret_key = genkey(key_length)
-
-([M1,M2,S],kF,dummy,prime) = secret_key
-
-string_1 = "Thanhminh"
-string_2 = "Th**hminh"
-
-def createIndexVector(string, kF):
-    for i in string_1:
-        print(">>",hashF(kF,i))
-
-p = np.array([1/3,0, 0, 1/11,1/7])
-q = np.array([33,7, 11, 11, 17])
-
-print(">> sk[S]", S)
-print(">> sk[M1]", M1)
-invert_M1 = np.linalg.inv(M1).reshape([key_length,key_length])
-transpose_M1 = np.transpose(M1).reshape([key_length,key_length])
-print(">> inverted sk[M1]",invert_M1)
-print(">> transpose sk[M1]",transpose_M1)
-print(">> inner product M_T M_1:",np.inner(transpose_M1,invert_M1))
-print("------------")
-print(">> sk[M2]", M2)
-invert_M2 = np.linalg.inv(M2).reshape([key_length,key_length])
-transpose_M2 = np.transpose(M2).reshape([key_length,key_length])
-print(">> inverted sk[M2]",invert_M2)
-print(">> transpose sk[M2]",transpose_M2)
+SK = genkey(k)
 
 
-def splitIndex(arr):
-    arr_a = []
-    arr_b = []
-    for i,vals in enumerate(S):
-        if vals == 0:
-            vals_a = vals_b = arr[i]
-            arr_a.append(vals_a)
-            arr_b.append(vals_b)
+(sk,kF,Dummies,Primes) = SK  
+print(Primes)
+(M1,M2,S) = sk
 
-        else: 
-            vals_a = 0.5*arr[i]+0.2
-            vals_b = 0.5*arr[i]-0.2
-            arr_a.append(vals_a)
-            arr_b.append(vals_b)
-    return [arr_a,arr_b]
+Wi =[["thanhminhabcxd","thplhmiwh","temp"],["math","html","thanhminh"],["thanhminh"]]
+Qj =["thanhmin****xd"]
 
-def splitQuery(arr):
-    arr_a = []
-    arr_b = []
-    for i,vals in enumerate(S):
-        if vals == 1:
-            vals_a = vals_b = arr[i]
-            arr_a.append(vals_a)
-            arr_b.append(vals_b)
+TQj= trapdoor(Qj,SK)
+for x,val in enumerate(Wi):
+    print("------ Search W_{} with TQj----------".format(x))
+    Ii= build_index(val,SK)
+    query_result = []
+    search_matrix = knn.Search(Ii, TQj)
+    result_query=[]
+    or_column = 0
+    and_column = 1
+    result_abs = np.round_(abs(np.int64(search_matrix) - np.float64(search_matrix)),decimals=6)
+    print(">>abs", result_abs)
+    for i in result_abs:
+        row_temp =[]
+        or_row = 0
+        and_row = 0
+        for j in i:
+            if ((1 >= j >= 0.99545) or (0 <= j <= 0.00045)): 
+                row_temp.append(1)
+                or_row = or_row or 1
+                and_row = and_row or 1
+            else: 
+                row_temp.append(0)
+                or_row = or_row or 0
+                and_row = and_row or 0
+        result_query.append(row_temp)
+        or_column = or_column or or_row
+        and_column = and_column and and_row
+    print(">> query",result_query," OR query:", bool(or_column), "| AND query", bool(and_column) )
 
-        else: 
-            vals_a = 0.5*arr[i]+0.1
-            vals_b = 0.5*arr[i]-0.1
-            arr_a.append(vals_a)
-            arr_b.append(vals_b)
-    return [arr_a,arr_b]
 
-# index
 
-p_split = splitIndex(p)
-print(">>p_split",p_split)
-
-p_a = np.matmul(M1,p_split[0])
-p_b = np.matmul(M2,p_split[1])
-print(">> p(p_a,p_b):", p_a,"-",p_b)
-
-print("-------------------")
-q_split = splitQuery(q)
-print(">>q_split", q_split)
-q_a = np.matmul(np.transpose(invert_M1),q_split[0])
-q_b = np.matmul(np.transpose(invert_M2),q_split[1])
-print(">> q(q_a,q_b):",q_a,"-",q_b)
-print("q_a shape:",np.shape(q_a))
-print("q_b shape:",np.shape(q_b))
-
-print(">>inner pai - qai:", np.inner(p_a,q_a))
-print(">>inner pbi - qbi:", np.inner(p_b,q_b))
-print(">>inner sum", np.inner(p_b,q_b) + np.inner(p_a,q_a))
-print(">> inner p - q:", np.inner(p,q))
 
 
 
