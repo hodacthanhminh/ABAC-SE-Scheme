b471185bae03ead20f1207808a4c8e5edd302203
hodacthanhminh <hodacthanhminh@gmail.com>
2022-05-30 03:04:56 -0700

    change genkey to class

--- genkey.py
+++ genkey.py
@@ -1,11 +1,41 @@
-import secrets
-import knn
+from copyreg import pickle
+from knn import *
 from CONSTANT import *
 from utils import *
+from pyspark import cloudpickle
+import pickle
 
-def genkey(k):
-    sk = knn.Key(d)
-    Dummies = createDummies(L)
-    Primes = createPrimes(L)  
-    KF = createHashKey(Dummies)
-    return (sk,KF,Dummies, Primes)
+
+class genkey:
+    def __init__(self, secParam, lengWord):
+        self.secParam = secParam
+        self.lengWord = lengWord
+        self.sk = knn.Key(self.secParam)
+        self.dummies = createDummies(self.lengWord)
+        self.primes = createPrimes(self.lengWord)
+        self.kf = createHashKey(self.dummies)
+
+    def writeFile(self):
+        with open('genKey.txt', 'wb') as filehandle:
+            cloudpickle.dump(obj={'sk': self.sk, 'dummies': self.dummies,
+                                  'primes': self.primes, 'kf': self.kf}, file=filehandle)
+            filehandle.close()
+
+    def readFile():
+        with open('genKey.txt', 'rb') as filehandle:
+            data = pickle.load(filehandle)
+            filehandle.close()
+
+        sk = data['sk']
+        primes = data['primes']
+        dummies = data['dummies']
+        kf = data['kf']
+        return (sk, dummies, primes, kf)
+
+    def main(self):
+       self.writeFile()
+
+
+if __name__ == "__main__":
+    createKey = genkey(d, L)
+    createKey.main()
