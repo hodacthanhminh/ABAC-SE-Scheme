e0046b0c07a15458d6d4ad66492aa4140e01e157
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2022-06-15 02:00:13 -0700

    update Genkey

--- se/genkey.py
+++ se/genkey.py
@@ -1,6 +1,8 @@
 # libs
 import json
 import pandas as pd
+import os
+import os.path
 # class/funcs
 from .knn import KNN
 from .utils import create_dummies, create_primes, create_hash_key, NumpyArrayEncoder
@@ -15,17 +17,19 @@ class GenKey:
         self.primes = create_primes(self.lengWord)
         self.kf = create_hash_key(self.dummies)
 
-    def write_key(self, key_id):
-
-        with open('./local/key/key-{}.json'.format(key_id), 'w') as filehandle:
+    def write_key(self, key_path, key_id):
+        path = os.path.join(os.getcwd(), key_path, 'key{}.json'.format(key_id))
+        with open(path, 'w') as filehandle:
             sk_str = json.dumps({'sk': self.sk, 'dummies': self.dummies, 'primes': self.primes,
                                 'kf': str(self.kf, 'latin1')}, cls=NumpyArrayEncoder)
-            data = json.dumps([{'id': 'key-{}'.format(key_id), 'data': sk_str}])
+            data = json.dumps([{'id': 'key{}'.format(key_id), 'data': sk_str}])
             filehandle.write(data)
+            filehandle.close()
 
 
-def read_key(key_id) -> tuple:
-    key = pd.read_json('./local/key/key-{}.json'.format(key_id), orient='records')
+def read_key(key_path) -> tuple:
+    path = os.path.join(os.getcwd(), key_path)
+    key = pd.read_json(path, orient='records')
     data = json.loads(key['data'][0])
     sk = tuple(data['sk'])
     primes = data['primes']
