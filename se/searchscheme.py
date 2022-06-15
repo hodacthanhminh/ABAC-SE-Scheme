a187b84d9bdc7b2ec68a9b27b2e41f87ccb825ae
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2022-06-15 03:54:17 -0700

    update se.searchscheme class PSE

--- se/searchscheme.py
+++ se/searchscheme.py
@@ -8,47 +8,48 @@ from .trapdoor import TrapDoor
 
 
 class PSE:
-    def __init__(self, secParams: int, lengWord: int, key_id: str):
+    def __init__(self, secParams: int, lengWord: int):
         self.d = secParams
         self.L = lengWord
         self.Result = []
-        self.SK = read_key(key_id)
+
+    def set_key(self, key_path):
+        self.SK = read_key(key_path)
 
     def set_index(self, index):
         self.Index = index['encrypt_index']
         self.File = index['id']
 
-    def createKey(self):
-        initkey = GenKey(self.d, self.L)
-        key_id = input('Create new key id:')
-        initkey.write_key(key_id)
+    def set_trapdoor(self, data):
+        self.TQj = data
 
     def get_trapdoor(self):
-        trapdoor = TrapDoor(self.SK, self.basic)
-        return trapdoor.create_trapdoor(self.Query)
+        return self.TQj
 
-    def set_scheme(self):
-        basic = input("Build Trapdoor '{'Basic: 1 , Advance: 2}]:")
-        if int(basic) == 1:
+    def create_key(self, key_path, key_id):
+        initkey = GenKey(self.d, self.L)
+        initkey.write_key(key_path, key_id)
+
+    def create_trapdoor(self):
+        trapdoor = TrapDoor(self.SK, self.basic)
+        self.TQj = trapdoor.create_trapdoor(self.Query)
+        return self.TQj
+
+    def set_detail(self, basic=True, query=True):
+        if basic:
             self.basic = True
         else:
             self.basic = False
-
-    def set_query(self):
-        query = input("Build Trapdoor '{'AND: 1 , OR: 2}]:")
-        if int(query) == 1:
+        if query:
             self.andQ = True
         else:
             self.andQ = False
-
-    def insert_query(self):
-        self.set_scheme()
-        print(">> Build Trapdoor as basic:", self.basic)
-        val = input("Search Keyword:")
-        self.Query = val.split(" ")
-        self.set_query()
-        self.TQj = self.get_trapdoor()
         self.Search = Search(self.andQ, self.basic)
+
+    def insert_query(self, query):
+        self.Query = query
+        self.create_trapdoor()
+        self.Result = []
 
     def set_search(self):
         for i, Ii in enumerate(self.Index):
@@ -56,7 +57,6 @@ class PSE:
             try:
                 if (self.Search.search_result(Im, self.TQj)):
                     self.Result.append(self.File[i])
-
             except:
                 print("{} >> broken".format(i))
                 continue
