f45a18c583b0c5457959a326bd01fd5c745da661
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2022-06-15 01:59:35 -0700

    create Upload Index

---
+++ cosmosInteraction/uploadIndex.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import json
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+parent_dir_name = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(parent_dir_name)
+
+dotenv_path = join(dirname(__file__), '.env.prod')
+load_dotenv(dotenv_path)
+ENDPOINT = os.environ.get("ENDPOINT")
+KEY = os.environ.get("KEY")
+DATABASE_NAME = os.environ.get("DATABASE_NAME")
+INDEX_CONTAINER = os.environ.get("INDEX_CONTAINER")
+
+
+if __name__ == "__main__":
+    from cosmosMethod import CosmosClass
+    cosmos_instance = CosmosClass(ENDPOINT, KEY, DATABASE_NAME)
+    cosmos_instance.set_container(INDEX_CONTAINER)
+    path = os.path.join(os.getcwd(), 'storage', '256', 'index')
+    for filename in os.listdir(path):
+        with open(os.path.join(path, filename), 'r') as f:
+            json_data = json.load(f)
+        f.close()
+        cosmos_instance.create_document(json_data)
